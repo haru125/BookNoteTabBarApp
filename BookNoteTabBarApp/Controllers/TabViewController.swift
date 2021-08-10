89d73294a800f8abccd8a51046e75746014126a3
haru125 <jhonathan1471@gmail.com>
2021-08-11 07:59:36 +0900

    first commit

---
+++ BookNoteTabBarApp/Controllers/TabViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabViewController.swift
+//  BookNoteTabBarApp
+//
+//  Created by Satoshi Ota on 2021/08/09.
+//
+
+import UIKit
+
+class TabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
